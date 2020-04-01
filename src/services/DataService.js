a3c31988fd4954a4751c0d40f6d0e842d4763a60
Alan Moratorio <amoratorio@siacseguridad.com>
2020-04-01 16:04:23 -0300

    bastantes cosas

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -8,9 +8,12 @@ export default class DataService {
     this.baseUrl = AppSettings.API_ENDPOINT;      
   }
 
-  getProfesionales(){
-    const url = this.baseUrl+'profesionales';
+  getProfesionales(fecha){
+    const url = this.baseUrl+'profesionales?start_date='+fecha;
     return(axios.get(url));
   }
-
+  getTurnosDiaProfesional(dia,profesional){
+    const url = this.baseUrl+'turnos-reservados?start_date='+dia+' 00:00&end_date='+dia+' 23:59&profe='+profesional;
+    return(axios.get(url));
+  }
 }
--- src/services/settings.js
+++ src/services/settings.js
@@ -1,8 +1,8 @@
 export class AppSettings {
 
 	//public static API_ENDPOINT='http://192.168.0.5:8000';
-	static API_ENDPOINT='http://localhost:8000/api/';
-	//public static API_ENDPOINT='http://localhost:8001';
+	//static API_ENDPOINT='http://10.1.0.239:8000/api/';
+	static API_ENDPOINT='http://health-management.com/';
 	//static API_ENDPOINT='http://192.168.1.223/backend/';
 	//public static API_ENDPOINT='http://35.198.33.205/api/v1';
 	
